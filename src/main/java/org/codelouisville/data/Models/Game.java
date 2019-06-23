c20a6e0f61e0dcd44c67515b1d64f7c3c7f2caf1
Ryan Sallee <rcsall11@g.holycross.edu>
2019-06-23 12:05:25 -0400

    Added logic diretory

--- src/main/java/org/codelouisville/data/Models/Game.java
+++ src/main/java/org/codelouisville/data/Models/Game.java
@@ -4,6 +4,9 @@ public class Game {
     private String gameTime;
     private String homeTeam;
     private String awayTeam;
+    private String homeCity;
+    private String homeState;
+    private Boolean isDomeStadium;
     private int homeScore;
     private int awayScore;
     private int homeYards;
