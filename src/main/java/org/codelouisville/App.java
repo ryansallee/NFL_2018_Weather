3584d9ad3b607b67823bd3ddb4ef4bc0d6b127ff
Ryan Sallee <rcsall11@g.holycross.edu>
2019-07-09 00:36:11 -0400

    Working with JavaFx to show data.

--- src/main/java/org/codelouisville/App.java
+++ src/main/java/org/codelouisville/App.java
@@ -4,16 +4,50 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.stage.Stage;
+import org.codelouisville.Logic.DBFunctions;
+import org.codelouisville.Logic.DbSeed;
+import org.codelouisville.Logic.Queries;
+import org.codelouisville.Logic.ReadCsv;
+import org.codelouisville.Models.Game;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
+    private static DBFunctions getDbFunctions() {
+        return dbFunctions;
+    }
+    private static Queries getQueries() {
+        return queries;
+    }
+    private static DBFunctions dbFunctions = new DBFunctions();
+    private static Queries queries = new Queries(dbFunctions);
     private static Scene scene;
+    private static List<Game> games = setGames();
+
+    private static List<Game> setGames(){
+        List<Game> gamesInner = null;
+        if(getQueries().checkDb() <=0) {
+            try {
+                gamesInner = ReadCsv.readingCSV(queries);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hey! We're seeding the db!");
+            DbSeed dbSeed = new DbSeed(getDbFunctions());
+            dbSeed.seed(gamesInner,queries);
+            gamesInner = getQueries().getGamesfromDb();
+        } else{
+            System.out.println("We didn't need to seed the db!");
+            gamesInner = getQueries().getGamesfromDb();
+        }
+        return gamesInner;
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -31,7 +65,44 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+    public static XYChart.Series<Number, Number> getScatter(){
+        //ObservableList<XYChart.Series<Double, Integer>> data = FXCollections.observableArrayList();
+        XYChart.Series<Number, Number> tempScore = new XYChart.Series<Number, Number>();
+        tempScore.setName("Temperature Scores");
+/*        games.forEach(g ->
+                new XYChart.Data<Number, Number>(
+                        g.getTemperature(),
+                        g.getHomeScore()
+                ));
+        games.forEach(g -> new XYChart.Data<Number, Number>(
+                g.getTemperature(),
+                g.getAwayScore()
+        ));*/
+        for (Game game: games) {
+
+            tempScore.getData().add(new XYChart.Data<Number, Number>(
+                    game.getTemperature(),
+                    game.getHomeScore()
+            ));
+        }
+        return tempScore;
+        /*data.add(tempScore);
+        data.forEach(d -> System.out.println(d.toString()));
+        return data;*/
+    }
+
+    public static ScatterChart<Number, Number> createScatter(){
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        ScatterChart sc = new ScatterChart<Number, Number>(xAxis, yAxis);
+        sc.getData().addAll(getScatter());
+        sc.setTitle("2019 Temperature and Total Game Scores");
+        return sc;
+    }
+
     public static void main(String[] args) {
+
         launch();
     }
 
--- src/main/java/org/codelouisville/PrimaryController.java
+++ src/main/java/org/codelouisville/PrimaryController.java
@@ -1,9 +1,38 @@
 package org.codelouisville;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import org.w3c.dom.events.MouseEvent;
 
 public class PrimaryController {
+
+    @FXML
+    private ScatterChart<Number, Number> temperatureChart;
+
+    @FXML
+    NumberAxis xAxis;
+    @FXML
+    NumberAxis yAxis;
+
+    public void loadTempChart(ActionEvent event){
+
+       XYChart.Series s1 = new XYChart.Series();
+       s1.setName("MAN");
+       s1.getData().add(new XYChart.Data(2, 3));
+       temperatureChart.getData().add(App.getScatter());
+    }
+
+
+    public PrimaryController(){}
 
     @FXML
     private void switchToSecondary() throws IOException {
