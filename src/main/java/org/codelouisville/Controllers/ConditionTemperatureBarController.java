7f1ac742a644a7400cb7e2cff43de8f8ef168a4b
Ryan Sallee <rcsall11@g.holycross.edu>
2019-07-13 14:02:14 -0400

    Able to display data with conditionTemperatureBarController.

--- src/main/java/org/codelouisville/Controllers/ConditionTemperatureBarController.java
+++ src/main/java/org/codelouisville/Controllers/ConditionTemperatureBarController.java
@@ -22,17 +22,20 @@ public class ConditionTemperatureBarController extends BaseChartController {
     @FXML
     @Override
     void loadHomeData(ActionEvent event) {
+        checkForData("Home Team");
         conditionTemperatureBarChart.getData().add(getChartData("Home Team"));
     }
     @FXML
     @Override
     void loadAwayData(ActionEvent event) {
-
+        checkForData("Away Team");
+        conditionTemperatureBarChart.getData().add(getChartData("Away Team"));
     }
     @FXML
     @Override
     void loadCombinedData(ActionEvent event) {
-
+        checkForData("Total Game");
+        conditionTemperatureBarChart.getData().add(getChartData("Total Game"));
     }
 
     @Override
@@ -54,9 +57,9 @@ public class ConditionTemperatureBarController extends BaseChartController {
     }
 
     @Override
-    XYChart.Series getChartData(String awayHomeTotal) {
+    XYChart.Series<String, Double> getChartData(String awayHomeTotal) {
         XYChart.Series<String, Double> averages = new XYChart.Series<>();
-        List<String> categories = Arrays.asList("Dome", "No Precipitation", "Precipitation");
+        List<String> categories = Arrays.asList("Dome", "No Precip", "Precip");
         int endOfRange;
         if(awayHomeTotal.equals("Home Team")){
             for(int i = 20; i <100; i+=10){
@@ -64,15 +67,60 @@ public class ConditionTemperatureBarController extends BaseChartController {
                 averages.setName(awayHomeTotal);
                 for(String category: categories)
                 {
-                    if(category.equals("Dome")){
+                    if(category.equals("Dome") && i == 70){
                         averages.getData().add(new XYChart.Data<>(
                                 category,
-                                getAverageHome(category, 70, 80)
+                                getAverageHome(category, i, endOfRange)
                         ));
-                    } else {
+                    } else if(category.equals("Dome")){}
+                    else {
                         averages.getData().add(new XYChart.Data<>(
                                 String.format("%d-%d %s", i, endOfRange, category),
                                 getAverageHome(category, i, endOfRange)
+                        ));
+                    }
+                }
+            }
+        }
+        if(awayHomeTotal.equals("Away Team")){
+            for(int i = 20; i <100; i+=10){
+                endOfRange = i + 10;
+                averages.setName(awayHomeTotal);
+                for(String category: categories)
+                {
+                    if(category.equals("Dome") && i == 70){
+                        averages.getData().add(new XYChart.Data<>(
+                                category,
+                                getAverageAway(category, i, endOfRange)
+                        ));
+                    } else if(category.equals("Dome")){}
+                    else {
+                        averages.getData().add(new XYChart.Data<>(
+                                String.format("%d-%d %s", i, endOfRange, category),
+                                getAverageAway(category, i, endOfRange)
+                        ));
+                    }
+                }
+            }
+        }
+        if(awayHomeTotal.equals("Total Game")){
+            for(int i = 20; i <100; i+=10){
+                endOfRange = i + 10;
+                averages.setName(awayHomeTotal);
+                for(String category: categories)
+                {
+                    if(category.equals("Dome") && i == 70){
+                        averages.getData().add(new XYChart.Data<>(
+                                category,
+                                getAverageAway(category, i, endOfRange) +
+                                        getAverageHome(category, i, endOfRange)
+                        ));
+                    } else if(category.equals("Dome")){}
+                    else {
+                        averages.getData().add(new XYChart.Data<>(
+                                String.format("%d-%d %s", i, endOfRange, category),
+                                getAverageAway(category, i, endOfRange) +
+                                        getAverageHome(category, i, endOfRange)
                         ));
                     }
                 }
@@ -82,10 +130,26 @@ public class ConditionTemperatureBarController extends BaseChartController {
     }
 
     private Double getAverageHome(String category, int i, int endOfRange) {
-        Double average = 1.0;
+        Double average = 0.0;
         if (category.equals("Dome")){
             average = games.stream()
                     .filter(Game::getDomeStadium)
+                    .mapToDouble(Game::getHomeScore)
+                    .average()
+                    .orElse(0.0);
+        } else if(category.equals("No Precip")){
+            average = games.stream()
+                    .filter(g -> g.getTemperature() > i && g.getTemperature() < endOfRange)
+                    .filter(g -> !g.getDomeStadium())
+                    .filter(g -> !g.getWeatherCondition().contains("Rain"))
+                    .filter(g-> !g.getWeatherCondition().contains("Drizzle"))
+                    .mapToDouble(Game::getHomeScore)
+                    .average()
+                    .orElse(0.0);
+        } else if(category.equals("Precip")) {
+            average = games.stream()
+                    .filter(g -> g.getTemperature() > i && g.getTemperature() < endOfRange)
+                    .filter(g -> g.getWeatherCondition().contains("Rain") || g.getWeatherCondition().contains("Drizzle"))
                     .mapToDouble(Game::getHomeScore)
                     .average()
                     .orElse(0.0);
@@ -93,5 +157,32 @@ public class ConditionTemperatureBarController extends BaseChartController {
         return average;
     }
 
+    private Double getAverageAway(String category, int i, int endOfRange) {
+        Double average = 0.0;
+        if (category.equals("Dome")){
+            average = games.stream()
+                    .filter(Game::getDomeStadium)
+                    .mapToDouble(Game::getAwayScore)
+                    .average()
+                    .orElse(0.0);
+        } else if(category.equals("No Precip")){
+            average = games.stream()
+                    .filter(g -> g.getTemperature() > i && g.getTemperature() < endOfRange)
+                    .filter(g -> !g.getDomeStadium())
+                    .filter(g -> !g.getWeatherCondition().contains("Rain"))
+                    .filter(g-> !g.getWeatherCondition().contains("Drizzle"))
+                    .mapToDouble(Game::getAwayScore)
+                    .average()
+                    .orElse(0.0);
+        } else if(category.equals("Precip")) {
+            average = games.stream()
+                    .filter(g -> g.getTemperature() > i && g.getTemperature() < endOfRange)
+                    .filter(g -> g.getWeatherCondition().contains("Rain") || g.getWeatherCondition().contains("Drizzle"))
+                    .mapToDouble(Game::getAwayScore)
+                    .average()
+                    .orElse(0.0);
+        }
+        return average;
+    }
 
 }
