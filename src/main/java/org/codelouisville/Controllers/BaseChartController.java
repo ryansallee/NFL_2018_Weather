9acfc90901ae4042083ef96b72561773b905054f
Ryan Sallee <rcsall11@g.holycross.edu>
2019-07-22 19:54:56 -0400

    Updated styles and comments further.

--- src/main/java/org/codelouisville/Controllers/BaseChartController.java
+++ src/main/java/org/codelouisville/Controllers/BaseChartController.java
@@ -11,9 +11,12 @@ import java.util.List;
 
 import static org.codelouisville.App.getQuery;
 
+//Abstract class to that allows for polymorphism as all of the concrete classes except HomeController directly inherit this class either.
 abstract class BaseChartController extends BaseController {
+    //Fields
     static final List<Game> games = getQuery().getGamesfromDb();
 
+    //Abstract methods to be overriden. All chart controllers must implement these classes.
     @FXML
     abstract void loadHomeData(ActionEvent event);
     @FXML
@@ -24,11 +27,14 @@ abstract class BaseChartController extends BaseController {
     abstract void checkForData(String seriesName);
     abstract void clearChart(ActionEvent event);
 
+    //All chart classes have a "Return to Home" button. It is implemented here as not to violate DRY.
     @FXML
     private void switchToHome() throws IOException {
         App.setRoot("home");
     }
 
+    //All chart classes except ScatterController use these methods below to obtain overall averages
+    //so they are implemented in this class and inherited.
     double getOverallHomeAverage() {
         return games.stream()
                 .mapToDouble(Game::getHomeScore)
--- src/main/java/org/codelouisville/Controllers/BaseController.java
+++ src/main/java/org/codelouisville/Controllers/BaseController.java
@@ -9,13 +9,16 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+//Abstract class that provides basic functionality to all controllers either directly or through a chain of inheritance
 abstract class BaseController {
+    //Both methods below are used everywhere in the app so they are implemented here.
     @FXML
     private void exit() {
         Platform.exit();
         System.exit(1);
     }
 
+    //The DarkSky "Powered By DarkSky" logo is required by DarkSky, and it must navigate to their "Powered By" page.
     @FXML
     private void goToDarkSky(MouseEvent mouseEvent) {
         try {
